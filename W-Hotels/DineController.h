de680e0526e0b515e0fe2d9fb42f083b862c899a
Roberto Hernandez <robhe@mac13.cs.okstate.edu>
2014-11-23 22:13:32 -0600

    Updated Dine

---
+++ W-Hotels/DineController.h
@@ -0,0 +1,14 @@
+//
+//  UIViewController+DineController.h
+//  W-Hotels
+//
+//  Created by HERNANDEZ ROBERTO H on 11/23/14.
+//  Copyright (c) 2014 Team Silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DineController : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *backgroundImage;
+
+@end
---
+++ W-Hotels/DineController.m
@@ -0,0 +1,28 @@
+//
+//  UIViewController+DineController.m
+//  W-Hotels
+//
+//  Created by HERNANDEZ ROBERTO H on 11/23/14.
+//  Copyright (c) 2014 Team Silver. All rights reserved.
+//
+
+#import "DineController.h"
+
+@implementation DineController: UIViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.backgroundImage.image = [UIImage imageNamed:@"Images/Dine/DinePicture.jpg"];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
