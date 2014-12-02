9adfb48725bdc9d3bbcd811ff1829173d82bc274
O'NEIL AUSTIN M <austimo@mac02.cs.okstate.edu>
2014-12-02 15:34:40 -0600

    Added the "indulDge" view

---
+++ W-Hotels/IndulgeController.h
@@ -0,0 +1,23 @@
+//
+//  IndulgeController.h
+//  W-Hotels
+//
+//  Created by O'NEIL AUSTIN M on 12/2/14.
+//  Copyright (c) 2014 Team Silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IndulgeController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *blissButton;
+@property (weak, nonatomic) IBOutlet UIButton *fitButton;
+@property (weak, nonatomic) IBOutlet UIButton *blissFavorite;
+@property (weak, nonatomic) IBOutlet UIButton *fitFavorite;
+@property (weak, nonatomic) IBOutlet UIImageView *titleImage;
+@property (weak, nonatomic) IBOutlet UIImageView *blissText;
+@property (weak, nonatomic) IBOutlet UIImageView *dividerImage;
+@property (weak, nonatomic) IBOutlet UIImageView *fitText;
+@property (weak, nonatomic) IBOutlet UIImageView *blissSpaExtended;
+@property (weak, nonatomic) IBOutlet UIImageView *fitExtended;
+
+@end
---
+++ W-Hotels/IndulgeController.m
@@ -0,0 +1,78 @@
+//
+//  IndulgeController.m
+//  W-Hotels
+//
+//  Created by O'NEIL AUSTIN M on 12/2/14.
+//  Copyright (c) 2014 Team Silver. All rights reserved.
+//
+
+#import "IndulgeController.h"
+
+@interface IndulgeController ()
+
+@property BOOL isExtended;
+@end
+
+@implementation IndulgeController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+- (IBAction)expand:(id)sender
+{
+    self.isExtended = ! self.isExtended;
+    if(self.isExtended)
+    {
+        self.fitFavorite.hidden=true;
+        self.blissFavorite.hidden=true;
+        self.fitText.hidden=true;
+        self.blissText.hidden=true;
+        self.dividerImage.hidden=true;
+        self.titleImage.hidden=true;
+        if(sender == self.blissButton)
+        {
+            self.blissSpaExtended.hidden=false;
+            self.fitButton.hidden=true;
+        }
+        else
+        {
+            self.fitExtended.hidden=false;
+            self.blissButton.hidden=true;
+        }
+        [self.fitButton setImage:[UIImage imageNamed:@"Arrow-up-button.png"] forState:UIControlStateNormal];
+        [self.blissButton setImage:[UIImage imageNamed:@"Arrow-up-button.png"] forState:UIControlStateNormal];
+    }
+    else
+    {
+        self.fitFavorite.hidden = false;
+        self.blissFavorite.hidden = false;
+        self.fitButton.hidden = false;
+        self.blissButton.hidden = false;
+        self.fitText.hidden = false;
+        self.blissText.hidden = false;
+        self.dividerImage.hidden = false;
+        self.titleImage.hidden=false;
+        self.blissSpaExtended.hidden = true;;
+        self.fitExtended.hidden = true;
+        [self.fitButton setImage:[UIImage imageNamed:@"Arrow-down-button.png"] forState:UIControlStateNormal];
+        [self.blissButton setImage:[UIImage imageNamed:@"Arrow-down-button.png"] forState:UIControlStateNormal];
+    }
+}
+
+@end
